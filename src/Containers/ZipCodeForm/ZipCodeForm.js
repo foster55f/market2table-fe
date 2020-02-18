58df7d25e5a667b0ba49f1031aa14dcff7e272d3
John Adams <adamsjr8576@gmail.com>
2020-02-17 18:43:17 -0700

    Update handling errors to not allow input of more than 6 digits correctly

--- src/Containers/ZipCodeForm/ZipCodeForm.js
+++ src/Containers/ZipCodeForm/ZipCodeForm.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './ZipCodeForm.scss';
 import { addZipCode, addMarkets } from '../../actions';
 
 export const ZipCodeForm = ({ path }) => {
 
-  const [zipCode, setZipCode] = useState('');
+  const [zipCodeInput, setzipCodeInput] = useState('');
+  const [hasError, setHasError] = useState(false);
   const dispatch = useDispatch();
+  const zipCode = useSelector(state => state.zipCode);
 
   const handleSubmit = () => {
-    if(zipCode.length === 5) {
-      fetch(process.env.REACT_APP_BACKEND_URL + `/v1/data.svc/zipSearch?zip=${zipCode}`)
+    if(zipCodeInput.length === 5) {
+      fetch(process.env.REACT_APP_BACKEND_URL + `/v1/data.svc/zipSearch?zip=${zipCodeInput}`)
         .then(res => res.json())
         .then(markets => {
           let marketPromises = markets.results.map(market => {
@@ -47,8 +49,11 @@ export const ZipCodeForm = ({ path }) => {
         .then(markets => {
           dispatch(addMarkets(markets));
         });
-      dispatch(addZipCode(zipCode));
-      setZipCode('');
+      dispatch(addZipCode(zipCodeInput));
+      setzipCodeInput('');
+    } else {
+      setHasError(true);
+      setzipCodeInput('');
     }
   }
 
@@ -58,21 +63,29 @@ export const ZipCodeForm = ({ path }) => {
     }
 
   const handleChange = (value) => {
-    if (zipCode.length <= 4) {
-      setZipCode(value)
+    if (value.length > 5) {
+      value = value.slice(0, 5);
     }
+    setzipCodeInput(value);
+    setHasError(false);
   }
 
+  if (zipCode > 0) {
+    return (
+      <Redirect to='/markets' />
+    )
+  }
   return (
     <section className='zip-code-form-container' style={{backgroundImage: `linear-gradient(rgba(255, 255, 255, ${opacity}), rgba(255, 255, 255, ${opacity}))` }}>
       <form className='zip-code-form'>
         <label className="zip-code-input-label" htmlFor="zipCode">
-              Enter zip code to find farmer's markets near you:
+          Enter a zip code to find farmer's markets near you:
         </label>
-        <input onChange={(e) => handleChange(e.target.value)} value={zipCode} className="zip-code-input" type="number" placeholder="Zip Code..." id="zipCode" name="zipCode" />
-        <Link to='/markets'>
-          <button type='button' onClick={handleSubmit}>Find!</button>
-        </Link>
+        <input onChange={(e) => handleChange(e.target.value)} value={zipCodeInput} className="zip-code-input" type="number" placeholder="zip code..." id="zipCode" name="zipCode" />
+        <div className='error-message-container'>
+          <p hidden={!hasError} className='error-message'>Error: Please enter a 5 digit zip code</p>
+        </div>
+        <button type='button' onClick={handleSubmit} className='submit-zip-code-button'>Find!</button>
       </form>
     </section>
   )
