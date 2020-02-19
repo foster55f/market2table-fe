7f4c6cd39f94a701f56ef08d173d8dc3d4c0a5e7
John Adams <adamsjr8576@gmail.com>
2020-02-19 15:43:25 -0700

    Add mockVendorData to use until backend endpoint is up and running

---
+++ src/mockVendors.js
@@ -0,0 +1,32 @@
+export const mockVendors = [
+  {
+    name: 'Honey Farm',
+    description: 'Making the best honey',
+    image: 'https://imgur.com/a/rkU6TOd'
+  },
+  {
+    name: 'Cheese Creamery',
+    description: 'The best curddled cheese around',
+    image: 'https://imgur.com/a/rkU6TOd'
+  },
+  {
+    name: 'River Bend Farm',
+    description: 'Fresh produce and vegetables',
+    image: 'https://imgur.com/a/rkU6TOd'
+  },
+  {
+    name: 'Farm 5',
+    description: 'Fresh fruit, produce and roasted chilis',
+    image: 'https://imgur.com/a/rkU6TOd'
+  },
+  {
+    name: 'farm 6',
+    description: 'Baked goods and cookies',
+    image: 'https://imgur.com/a/rkU6TOd'
+  },
+  {
+    name: 'farm 7',
+    description: 'homemade pies and pastries',
+    image: 'https://imgur.com/a/rkU6TOd'
+  }
+]
