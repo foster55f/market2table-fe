5888d3548f1995cb7cec998d0f701cc2f3a9d866
John Adams <adamsjr8576@gmail.com>
2020-02-20 12:23:53 -0700

    Complete search input for vendors by name functionality

--- src/Containers/VendorSelectContainer/VendorSelectContainer.js
+++ src/Containers/VendorSelectContainer/VendorSelectContainer.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './VendorSelectContainer.scss';
 import VendorSelectCard from '../VendorSelectCard/VendorSelectCard';
+import { addVendors } from '../../actions';
 
 export const VendorSelectContainer = () => {
 
   const [vendorInput, setVendorInput] = useState('');
+  const [searchError, setSearchError] = useState(false);
+  const [vendors, setVendors] = useState([]);
   const allVendors = useSelector(state => state.vendors);
+  const dispatch = useDispatch();
 
-  const createVendorCards = vendors => {
-    return vendors.map(vendor => {
-      return (<VendorSelectCard name={vendor.name} />)
+  useEffect(() => {
+    setVendors(allVendors)
+  }, [])
+
+    const vendorSelectCards = vendors.map(vendor => {
+      return (<VendorSelectCard name={vendor.name} key={vendor.name}/>)
     });
+
+
+  const searchVendors = vendorInput => {
+    const filteredVendors = allVendors.filter(vendor => {
+      const vendorInputLowerCase = vendorInput.toLowerCase();
+      const vendorNameLowerCase = vendor.name.toLowerCase();
+      const splitName = vendorNameLowerCase.split(' ');
+      return splitName.includes(vendorInputLowerCase);
+    });
+    if (filteredVendors.length) {
+      setVendors(filteredVendors);
+    } else {
+      setSearchError(true);
+    }
   }
 
   return (
@@ -23,12 +44,15 @@ export const VendorSelectContainer = () => {
         <form className='search-vendors-form'>
           <input type='text' value={vendorInput} onChange={(e) => {setVendorInput(e.target.value)}} className='search-vendors-input' placeholder='Search Vendors...'/>
           <div className='search-vendors-button-container'>
-            <button type='button' className='search-vendors-button'>Search</button>
-            <button type='button' className='search-vendors-button'>Clear</button>
+            <button type='button' className='search-vendors-button' onClick={() => {searchVendors(vendorInput); setVendorInput('')}}>Search</button>
+            <button type='button' className='search-vendors-button' onClick={() => {setVendors(allVendors); setVendorInput(''); setSearchError(false)}}>Clear</button>
           </div>
         </form>
+        <div className="vendor-search-error-container">
+          <p className="vendor-search-error" hidden={!searchError}>No matches for vendor name: {vendorInput}</p>
+        </div>
         <section className='vendor-cards-section'>
-          {createVendorCards(allVendors)}
+          {vendorSelectCards}
         </section>
       </section>
       <button className='create-vendor-button'>Create New Vendor</button>
