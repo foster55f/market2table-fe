71bd4813faebae017b8dd62a270853d7c4000bfb
John Adams <adamsjr8576@gmail.com>
2020-02-20 11:24:07 -0700

    Add vendorInput local state to control the search vendors form

--- src/Containers/VendorSelectContainer/VendorSelectContainer.js
+++ src/Containers/VendorSelectContainer/VendorSelectContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -6,26 +6,29 @@ import './VendorSelectContainer.scss';
 import VendorSelectCard from '../VendorSelectCard/VendorSelectCard';
 
 export const VendorSelectContainer = () => {
+
+  const [vendorInput, setVendorInput] = useState('');
   const allVendors = useSelector(state => state.vendors);
-  const vendorSelectCards = allVendors.map(vendor => {
-    return (
-      <VendorSelectCard name={vendor.name} />
-    )
-  });
+
+  const createVendorCards = vendors => {
+    return vendors.map(vendor => {
+      return (<VendorSelectCard name={vendor.name} />)
+    });
+  }
 
   return (
     <div className='vendor-select-container'>
       <section className='vendor-list-section'>
         <h2 className='vendor-select-header'>Vendors</h2>
         <form className='search-vendors-form'>
-          <input type='text' className='search-vendors-input' placeholder='Search Vendors...'/>
+          <input type='text' value={vendorInput} onChange={(e) => {setVendorInput(e.target.value)}} className='search-vendors-input' placeholder='Search Vendors...'/>
           <div className='search-vendors-button-container'>
             <button type='button' className='search-vendors-button'>Search</button>
             <button type='button' className='search-vendors-button'>Clear</button>
           </div>
         </form>
         <section className='vendor-cards-section'>
-          {vendorSelectCards}
+          {createVendorCards(allVendors)}
         </section>
       </section>
       <button className='create-vendor-button'>Create New Vendor</button>
