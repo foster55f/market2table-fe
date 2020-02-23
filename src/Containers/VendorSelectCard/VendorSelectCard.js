61d0ddb0db0083761b49fe8b7f0c6bcdf6bd7fcb
John Adams <adamsjr8576@gmail.com>
2020-02-23 12:46:24 -0700

    Add getAllVendors functionalit on VenderSelectContainer

--- src/Containers/VendorSelectCard/VendorSelectCard.js
+++ src/Containers/VendorSelectCard/VendorSelectCard.js
@@ -3,15 +3,20 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './VendorSelectCard.scss';
+import { addSelectedVendor } from '../../actions';
 
-export const VendorSelectCard = ({ name }) => {
+export const VendorSelectCard = ({ name, vendor }) => {
+
+  const dispatch = useDispatch();
 
   return (
     <article className='vendor-select-article'>
       <h2 className='vendor-article-header'>{name}</h2>
       <section className='vendor-article-button-container'>
         <button className='vendor-article-markets-button'>Link to Markets</button>
-        <button className='vendor-article-edit-button'>Edit</button>
+        <Link to='/vendor/account/form' className='link-edit-vendor-form'>
+          <button className='vendor-article-edit-button' onClick={() => dispatch(addSelectedVendor(vendor))}>Edit</button>
+        </Link>
       </section>
     </article>
   )
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,11 +2,13 @@ import { zipCode } from '../reducers/zipCode';
 import { markets } from '../reducers/markets';
 import { selectedMarket } from '../reducers/selectedmarket';
 import { vendors } from '../reducers/vendors';
+import { selectedVendor } from '../reducers/selectedVendor';
 import { combineReducers } from 'redux';
 
 export const rootReducer = combineReducers({
   zipCode,
   markets,
   selectedMarket,
-  vendors
+  vendors,
+  selectedVendor
 });
