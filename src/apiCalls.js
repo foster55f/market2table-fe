73b6ac2ba74c779b706dc2ea6a0b9059aa4d203b
“Foster <fosterataylor@yahoo.com>
2020-02-24 14:19:52 -0700

    Delete edits in apiCalls and fix selectedVendorReducer test

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,17 +9,7 @@ export const getMarketsByZip = (zipCode) => {
             return response.json();
         });
 }
-
-// export const getVendorsByMarketId = (marketId) => {
-//     return fetch(process.env.REACT_APP_BACKEND_URL + `/graphql?query=/`)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw Error('Error fetching markets');
-//             }
-//             return response.json();
-//         });
-// }
-
+ 
 export const getAllVendors = () => {
     return fetch(process.env.REACT_APP_BACKEND_URL + '/graphql?query=query{vendors{id name description image_link products{id name description price}}}')
         .then(response => {
--- src/reducers/selectedVendorReducer.test.js
+++ src/reducers/selectedVendorReducer.test.js
@@ -3,22 +3,22 @@ import { selectedVendor } from './selectedVendor';
 describe('selectedVendor', () => {
   it('should return the initial state', () => {
     // Setup
-    const expected = '';
+    const expected = {};
 
     // Execution
-    const result = selectedVendor('', '');
+    const result = selectedVendor({}, {});
 
     // Expectation
     expect(result).toEqual(expected);
   });
 });
 
-it('should return a selectedvendor id string', () => {
+it('should return a selectedvendor object', () => {
     const mockAction = {
       type: 'ADD_SELECTED_VENDOR',
-      id: 2,
+      vendor:{name:'farmer', description:'selling'}
     }
-    const expected = mockAction.id;
-    const result = selectedVendor('', mockAction);
+    const expected = mockAction.vendor;
+    const result = selectedVendor({name:'farmer', description:'selling'}, mockAction);
     expect(result).toEqual(expected);
   });
