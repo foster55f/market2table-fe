123ad7d134517ef67f87a4ee3463605434bcb37a
John Adams <adamsjr8576@gmail.com>
2020-02-24 21:51:36 -0700

    Add address to InfoWindow on googlemaps

--- src/Containers/MapsContainer/MapsContainer.js
+++ src/Containers/MapsContainer/MapsContainer.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './MapsContainer.scss';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
@@ -11,7 +10,7 @@ const mapStyles = {
   height: '600px'
 };
 
-export const MapsContainer = ({ google }) => {
+export const MapsContainer = ({ history, google }) => {
 
   const [selectedPlace, setSelectedPlace] = useState({});
   const [activeMarker, setActiveMarker] = useState({});
@@ -40,18 +39,20 @@ export const MapsContainer = ({ google }) => {
           name={marketInfo.marketname}
           position={{ lat: marketInfo.latitude, lng: marketInfo.longitude }}
           key={marketInfo.id}
+          address={marketInfo.address}
         />)
     } else {
       return markets.map(market => {
         return (
           <Marker
               onClick={onMarkerClick}
-              name={market.marketname}
+              name={market.name}
               position={{ lat: market.latitude, lng: market.longitude }}
               key={market.id}
+              address={market.address}
             />
         )
-      })
+      });
     }
   }
 
@@ -100,6 +101,7 @@ export const MapsContainer = ({ google }) => {
           >
             <div className='info-window-container'>
               <h4>{selectedPlace.name}</h4>
+              <h4>{selectedPlace.address}</h4>
             </div>
           </InfoWindow>
         </Map>
