747b8a7cbb1fb7676e6dc0d5728b495fad546b97
John Adams <adamsjr8576@gmail.com>
2020-02-25 19:19:43 -0700

    Add tests to VendorSelectContainer

--- src/Containers/VendorSelectContainer/VendorSelectContainer.test.js
+++ src/Containers/VendorSelectContainer/VendorSelectContainer.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import VendorSelectContainer from './VendorSelectContainer';
 import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
 
 const mockState = {
     vendors: [{name:'vendorA', description:'selling', products:'apples'},{name:'vendorB', description:'sellin', products:'oranges'}],
@@ -19,6 +20,25 @@ describe('VendorSelectContainer', () => {
             addVendors={mockAddVendors}
           />);
         expect(wrapper).toMatchSnapshot();
-    })
+    });
+
+    it("Should load with an initial state of '' for the search vendors input", () => {
+      let wrapper = shallow(<VendorSelectContainer
+          vendors={mockState.vendors}
+          addVendors={mockAddVendors}
+        />);
+
+      expect(wrapper.find('.search-vendors-input').text()).toEqual('');
+    });
+
+    it("Should set input to '' on clear button click", () => {
+      let wrapper = shallow(<VendorSelectContainer
+          vendors={mockState.vendors}
+          addVendors={mockAddVendors}
+        />);
+      wrapper.find('.search-vendors-input').simulate('change', {target: { value: 'Farm'}});
+      wrapper.find('.search-clear-button').simulate('click');
+      expect(wrapper.find('.search-vendors-input').text()).toEqual('');
+    });
 
 });
